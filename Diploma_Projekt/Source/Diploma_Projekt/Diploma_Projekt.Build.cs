83ea314a9adaf8791506512283312122307b3bfd
Nick Buchhorn <sarttus@outlook.de>
2017-10-31 16:15:16 +0100

    Pathfinding update; Added  Graphics Menu

--- Diploma_Projekt/Source/Diploma_Projekt/Diploma_Projekt.Build.cs
+++ Diploma_Projekt/Source/Diploma_Projekt/Diploma_Projekt.Build.cs
@@ -8,7 +8,7 @@ public class Diploma_Projekt : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent","RHI" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
